fca2214906bd933290d2cdd13d90203026a7c860
Kyle Geske <stungeye@gmail.com>
2014-11-26 13:32:48 -0600

    Added requires to index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+  require('includes/database.php');
+  require('includes/user_message.php');
+  require('includes/user_helpers.php');
+
+?>
+
 <?php require('partials/header.php') ?>
 
 <p>List of links goes here.</p>

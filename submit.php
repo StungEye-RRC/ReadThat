e8496d614a72f71cf4b687b301402a10942d43e5
Kyle Geske <stungeye@gmail.com>
2014-11-26 13:32:14 -0600

    New submit form.

--- submit.php
+++ submit.php
@@ -1,19 +1,22 @@
+<?php
+  require('includes/database.php');
+  require('includes/user_message.php');
+  require('includes/user_helpers.php');
+
+?>
 <?php require('partials/header.php') ?>
 
     <form action="submit.php" method="post">
         <fieldset>
-            <h4>ReadThat Login</h4>
+            <h4>ReadThat Submission</h4>
             <p>
-              <input name="username" type="text" placeholder="User Name" />
+              <input name="title" type="text" placeholder="Link Title" />
             </p>
             <p>
-              <input name="password" type="password" placeholder="Password"/>
+              <input name="url" type="text" placeholder="http://somedomain.com/some_path/"/>
             </p>
             <p class="right">
-                <a href="register.php">Register New Account</a>
-            </p>
-            <p class="right">
-                <input name="submit" value="login" type="submit">
+                <input name="submit" value="submit" type="submit">
             </p>
         </fieldset>
     </form>

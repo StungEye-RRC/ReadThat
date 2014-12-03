655ba6c09e04c20f4cdd5355b010480d89deb590
Kyle Geske <stungeye@gmail.com>
2014-12-03 11:12:29 -0600

    Markup and styling added for up and down voting.

--- index.php
+++ index.php
@@ -14,13 +14,30 @@
     <ul>
       <?php foreach($links as $link): ?>
         <li>
-          <p class="title">
-            <a href="<?= $link['url'] ?>"><?= $link['title'] ?></a>
-          </p>
-          <p class="tagline">
-            Submitted <time title="<?= $link['created_at'] ?>" datetime="<?= $link['created_at'] ?>"><?= time_ago_in_words($link['created_at']) ?></time>
-            by <?= $link['username'] ?>.
-          </p>
+          <aside>
+            <header>
+              <form action="vote.php" method="post">
+                <input type="hidden" name="id" value="<?= $link['id'] ?>">
+                <input type="submit" name="up" value="&#9650;">
+              </form>
+            </header>
+            <p>5</p>
+            <footer>
+              <form action="vote.php" method="post">
+                <input type="hidden" name="id" value="<?= $link['id'] ?>">
+                <input type="submit" name="down" value="&#9660;">
+              </form>
+            </footer>
+          </aside>
+          <summary>
+            <p class="title">
+              <a href="<?= $link['url'] ?>"><?= $link['title'] ?></a>
+            </p>
+            <p class="tagline">
+              Submitted <time title="<?= $link['created_at'] ?>" datetime="<?= $link['created_at'] ?>"><?= time_ago_in_words($link['created_at']) ?></time>
+              by <?= $link['username'] ?>.
+            </p>
+          </summary>
         </li>
       <?php endforeach ?>
     </ul>
